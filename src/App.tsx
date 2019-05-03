9b4962637e86f5cb34844839833815e46c7bfdaf
matt-erhart <mattjerhart@gmail.com>
2019-05-03 13:12:09 -0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
 import * as React from 'react'
-import {DivStage} from './DivStage'
-export const App = () => (
-  <DivStage />
-)
-
+import { DivStage } from './DivStage'
+export const App = () => <DivStage />
