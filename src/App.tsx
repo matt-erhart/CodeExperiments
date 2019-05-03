fe282e98a977941b3751916ce7d3c74c41f2bf50
matt-erhart <mattjerhart@gmail.com>
2019-05-03 12:56:16 -0700

    css to fit to browser size

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import {DivStage} from './DivStage'
+export const App = () => (
+  <DivStage />
+)
+
---
+++ src/DivStage.tsx
@@ -0,0 +1,8 @@
+import * as react from 'react'
+import styled from 'styled-components'
+
+export const DivStage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 4px solid blue;
+`
