c0a5c6c3b6274bac290b7b22f77e5d568f3e046b
Matt Erhart <mattjerhart@gmail.com>
2019-05-03 18:35:36 -0700

    react-spring

--- src/ForceLayout.tsx
+++ src/ForceLayout.tsx
@@ -3,44 +3,26 @@ import * as React from 'react'
 import * as d3 from 'd3'
 import * as cola from 'webcola'
 const myWorker = new Worker('./LayoutWorker.js') // relative path to the source file, not the public URL
-
+import { Spring, animated, config } from 'react-spring/renderprops'
 // custom
 import { DivStage } from './DivStage'
+import throttle from 'lodash/throttle'
 
 const makeGraph = () => {
-  const nodeNumbers = d3.range(0, 100, 1)
+  const nodeNumbers = d3.range(0, 300, 1)
   const nodes = nodeNumbers.map(num => {
-    return { x: 300, y: 300, width: 10, height: 10 }
+    return { x: 500, y: 500, width: 30, height: 30 }
   })
-  const linkNumbers = d3.range(0, 100)
+  const linkNumbers = d3.range(0, 50)
   const links = linkNumbers.map(num => {
     return {
       source: Math.round(Math.random() * Math.max(...nodeNumbers)),
       target: Math.round(Math.random() * Math.max(...nodeNumbers)),
-      value: 2,
+      value: Math.random() * 100,
     }
   })
   return { nodes, links }
 }
-
-// const graph = {
-//   nodes: [
-//     { x: 350, y: 350, width: 101, height: 101 },
-//     { x: 350, y: 350, width: 10, height: 10 },
-//     { x: 350, y: 100, width: 10, height: 101 },
-//     { x: 350, y: 350, width: 101, height: 110 },
-//     { x: 350, y: 350, width: 10, height: 10 },
-//     { x: 350, y: 350, width: 10, height: 110 },
-//     { x: 350, y: 350, width: 110, height: 110 },
-//     { x: 350, y: 350, width: 110, height: 110 },
-//   ],
-//   links: [
-//     { source: 1, target: 2, value: 1 },
-//     { source: 2, target: 4, value: 8 },
-//     { source: 7, target: 5, value: 10 },
-//     { source: 5, target: 2, value: 6 },
-//   ],
-// }
 
 let graph = makeGraph()
 
@@ -65,39 +47,44 @@ export class ForceLayout extends React.Component<
     myWorker.onmessage = ({ data }) => {
       this.setState({ nodes: data })
     }
-    console.log('width, height: ', width, height)
   }
-  
+
   componentWillUnmount() {
-    // myWorker.terminate()
+    myWorker.terminate()
   }
+  log = () => console.log(!!this.state.nodes)
 
   render() {
     return (
       <DivStage ref={this.stage}>
         {/* <div>{this.state.alpha}</div> */}
-        {this.state.nodes &&
-          this.state.nodes.map((node, ix) => {
-            const left = node.x - node.width / 2
-            const top = node.y - node.height / 2
-            return (
-              <div
-                key={ix}
-                style={{
-                  position: 'absolute',
-                  transform: `translate(${left}px, ${top}px)`,
-                  width: node.width,
-                  height: node.height,
-                  border: '1px solid black',
-                }}
-              />
-            )
-          })}
+        {this.state.nodes && (
+          <Spring
+            native
+            to={{ nodes: this.state.nodes }}
+            config={{duration: 50}}
+          >
+            {({ nodes }) =>
+              this.state.nodes.map((node, ix) => {
+                const left = node.x - node.width / 2
+                const top = node.y - node.height / 2
+                return (
+                  <animated.div
+                    key={ix}
+                    style={{
+                      position: 'absolute',
+                      transform: `translate3d(${left}px, ${top}px, 0px)`,
+                      width: node.width,
+                      height: node.height,
+                      border: '2px solid black',
+                    }}
+                  />
+                )
+              })
+            }
+          </Spring>
+        )}
       </DivStage>
     )
   }
-}
-
-async function test () {
-    
 }
--- src/LayoutWorker.js
+++ src/LayoutWorker.js
@@ -1,16 +1,18 @@
 import * as cola from 'webcola'
 let layout = new cola.Layout()
+import throttle from 'lodash/throttle'
+const post = throttle(() => postMessage(layout.nodes()), 0) // send back result
 
 addEventListener('message', ({ data, timeStamp }) => {
   const { graph, width, height } = data
+
   layout
     .size([width, height])
     .nodes(graph.nodes)
     .links(graph.links)
-    .convergenceThreshold(0.01)
     .handleDisconnected(false) // handle disconnected repacks the components which would hide any drift
     .avoidOverlaps(true)
-    .jaccardLinkLengths(20, 5)
+    .jaccardLinkLengths(10, 5)
     .on(cola.EventType.start, e => {
       // this.setState({ nodes: [...this.layout.nodes()] })
     })
@@ -19,14 +21,12 @@ addEventListener('message', ({ data, timeStamp }) => {
       // console.log(e)
     })
     .on('tick', e => {
-      postMessage(layout.nodes()) // send back result
-
+      post()
       // this.setState({ nodes: [...this.layout.nodes()], alpha: e.alpha }, () =>
       //   console.log('SETSTATE')
       // )
       // this.forceUpdate()
       // console.log(e)
     })
-    .start()
+    .start(20, 0, 20)
 })
-
