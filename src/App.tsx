498894a95c4ec877f54b44d8d9f1f88c2da0613a
Matt Erhart <mattjerhart@gmail.com>
2019-05-24 18:45:51 -0700

    save

--- src/App.tsx
+++ src/App.tsx
@@ -22,39 +22,49 @@ const logit = e => {
 export const App = () => {
   const ref = useRef(null)
   const [coords, setCoords] = useState({ left: 0, top: 0 })
-  const scale = 1.2
+  const scale = 0.9
   return (
-    <div>
-      <div
-        ref={ref}
-        style={{
-          position: 'absolute',
-          left: '10px',
-          top: '10px',
-          margin: '10px',
-          width: '1000px',
-          height: '1000px',
-          outline: '1px solid black',
-          transform: `scale(${scale})`,
-        }}
-        onMouseDown={e => {
-          const bbox = ref.current.getBoundingClientRect()
-          const browserZoom =
-            Math.round((window.outerWidth / window.innerWidth) * 100) / 100
-          const transedX = Math.round((e.clientX - bbox.left) / scale)
-          const transedY = Math.round((e.clientY - bbox.top) / scale)
-          setCoords({left: transedX, top: transedY})
-          console.log('e.clientX-e.offsetX: ', transedX - e.nativeEvent.offsetX)
-        }}
-      >
+    <div style={{ transform: `scale(${scale})` }}>
+      <div style={{ transform: `scale(${scale})` }}>
+        <div style={{ height: '100px' }}>style</div>
         <div
+          ref={ref}
           style={{
-            outline: '1px solid blue',
             position: 'absolute',
-            left: coords.left,
-            top: coords.top,
+            left: '10px',
+            top: '10px',
+            margin: '10px',
+            width: '50vh',
+            height: '50vh',
+            outline: '1px solid black',
+            transform: `scale(${scale})`,
           }}
-        />
+          onMouseDown={e => {
+            const bbox = ref.current.getBoundingClientRect()
+            const browserZoom =
+              Math.round((window.outerWidth / window.innerWidth) * 100) / 100
+            const transedX = Math.round(
+              (e.clientX - bbox.left) / scale / scale / scale
+            )
+            const transedY = Math.round(
+              (e.clientY - bbox.top) / scale / scale / scale
+            )
+            setCoords({ left: transedX, top: transedY })
+            console.log(
+              'e.clientX-e.offsetX: ',
+              transedX - e.nativeEvent.offsetX
+            )
+          }}
+        >
+          <div
+            style={{
+              outline: '1px solid blue',
+              position: 'absolute',
+              left: coords.left,
+              top: coords.top,
+            }}
+          />
+        </div>
       </div>
     </div>
   )
